e96fb1f3f0ff9f72b3bc73c531624799908a893c
Simon Jackler <simon@apparts.se>
2024-12-10 09:29:10 +0100

    Fixes non optional variables

--- Sources/DocCHTMLExporter/HTML/ReferenceHTML.swift
+++ Sources/DocCHTMLExporter/HTML/ReferenceHTML.swift
@@ -90,7 +90,7 @@ extension DocCArchive.Reference {
         return ref.abstract.generateHTML(in: ctx)
         
       case .image(let ref):
-        return ref.alt?.htmlEscaped ?? ""
+        return ref.alt.htmlEscaped
 
       case .link(let ref):
         return ref.title.htmlEscaped
@@ -150,7 +150,7 @@ extension DocCArchive.ImageReference {
   func generateHTML(in ctx: DZRenderingContext) -> String {
     guard let variant = bestVariant(for: ctx.traits) else {
       assertionFailure("Got no image variant?")
-      return "<!-- invalid image ref -->\(alt?.htmlEscaped ?? "")"
+      return "<!-- invalid image ref -->\(alt.htmlEscaped)"
     }
     
     let media : String = {
@@ -173,7 +173,7 @@ extension DocCArchive.ImageReference {
     var ms = "<picture><source\(media) srcset='\(srcset)'>"
 
     ms += "<img"
-    if let alt = alt, !alt.isEmpty { ms += " alt='\(alt.htmlEscaped)'" }
+    if !alt.isEmpty { ms += " alt='\(alt.htmlEscaped)'" }
     ms += " srcset='\(srcset)'"
     ms += " src='\(url.htmlEscaped)'"
     if let size = variant.size { ms += " width='\(size.width)' height='auto'" }
